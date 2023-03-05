1b594df81cbe14e072cbcb1c42c4456cbca1f42b
elkewind <elke.windschitl@gmail.com>
2023-03-05 15:30:41 -0800

    Fixed mask problem, added longer aoi

--- mask_rast.R
+++ mask_rast.R
@@ -1,10 +1,16 @@
+library(sf)
+library(tmap)
+library(terra)
+library(tidyverse)
+library(cmocean)
+
 # Make an empty raster of 0.008 degree resolution
 x <- rast() # make an empty raster
 x
 
 crs(x) <- "EPSG:4326" # confirm WGS84
 crs(x)
-ext(x) <- c(-120.5, -119.45, 33.83, 34.49) # set extent
+ext(x) <- c(-120.65, -118.80, 33.85, 34.59) # set extent
 ext(x)
 res(x) <- c(0.008, 0.008) # set resolution
 res(x)
@@ -12,15 +18,35 @@ res(x)
 nrow(x) # identify how many rows we have
 ncol(x) # identify how many columns we have
 ncell(x) # identify how many total cells we have
-values(x) <- 1:ncell(x) # fill in values 1-10873 by row
+values(x) <- 1 # fill in values 1-10873 by row
+
+colors1 <- cmocean("diff")(5) # Bring in a palette
 
 tmap_mode("view")
 tm_shape(x) +
-  tm_raster(style = "cont",
-            breaks = seq(0, ncell(x), by = 1000),
-            title = "Cell ID",
-            palette = "RdPu")
+  tm_raster(palette = colors1)
 
 mask <- x
 
-terra::writeRaster(resampled_depth, "mask_rast.tif", filetype = "GTiff", overwrite = TRUE)
+boundaries <- st_read("/Users/elkewindschitl/Downloads/California_County_Boundaries/cnty19_1.shp") %>% filter(COUNTY_ABB == "SBA")
+
+boundaries <- st_transform(x = boundaries, crs = 4326)
+
+boundaries <- vect(boundaries)
+boundaries <- rasterize(x = boundaries, 
+                        y = mask)
+reclassification_matrix <- matrix(c(NaN, 1, 1, NaN), ncol = 2, byrow = TRUE)
+
+land_bounds <- classify(boundaries, rcl = reclassification_matrix)
+
+plot(land_bounds)
+
+mask <- mask * land_bounds
+
+tmap_mode("plot")
+tm_shape(mask) +
+  tm_raster(palette = colors1,
+            legend.show = FALSE)
+
+terra::writeRaster(mask, "mask_rast.tif", filetype = "GTiff", overwrite = TRUE)
+
